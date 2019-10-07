983d795dfac28fbcbf50221cd55c060ae7cad2b2
Pedro González-Santiago González <pedro.gonzalez@unitedcuisines.de>
2019-10-07 20:44:04 +0200

    Enable eslint and remove linter errors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
           Learn React
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
